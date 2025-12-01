1810fe94b720f5744ba959e67ffaa95a5fa5a572
Alex Howard <thezanke@gmail.com>
2025-12-01 11:10:47 -0500

    day1 part2

--- day1/day1.py
+++ day1/day1.py
@@ -1,67 +1,53 @@
-def get_input(file_path):
-    with open(file_path, 'r') as file:
-        return file.readlines()
-
-def solve(input_lines, start_location = 50):
+def solve(input_lines, start_location=50):
     curr = start_location
     z = 0
 
-    print("Starting location:", curr)
-
     for line in input_lines:
-        direction, distance = line[0], int(line[1:])
+        direction, chng = line[0], int(line[1:])
 
-        if direction == 'L':
-            distance = -distance
+        if direction == "L":
+            chng = -chng
 
-        curr = ((curr + distance) % 100 + 100) % 100
-        if (curr == 0):
+        curr = ((curr + chng) % 100 + 100) % 100
+        if curr == 0:
             z += 1
 
+    return z
 
-        print("Updated location:", curr)
 
-    print("Final location:", curr)
-
-    print("Zeroes:", z)
-
-def solve2(input_lines, start_location = 50):
+def solve2(input_lines, start_location=50):
     curr = start_location
     z = 0
 
-    print("Starting location:", curr)
-
     for line in input_lines:
-        direction, distance = line[0], int(line[1:])
+        direction, chng = line[0], int(line[1:])
+        dist = (curr if direction == "L" else 100 - curr) or 100
+        rem = 0
 
-        if direction == 'L':
-            if distance >= curr:
-                z += (distance - curr) // 100
-            distance = -distance
+        if chng >= dist:
+            z += 1
+            rem = chng - dist
+            if rem > 0:
+                z += rem // 100
 
-        if direction == 'R':
-            remaining = 100 - curr
-            if distance >= remaining:
-                z += (distance - remaining) // 100
+        if direction == "L":
+            chng = -chng
 
-        curr = ((curr + distance) % 100 + 100) % 100
+        curr = ((curr + chng) % 100 + 100) % 100
 
-        print("Updated location:", curr)
+    return z
 
-    print("Final location:", curr)
-    print("Zeroes:", z)
-        
 
-example = """L68
-L30
-R48
-L5
-R60
-L55
-L1
-L99
-R14
-L82""".splitlines()
+def get_input(file_path):
+    with open(file_path, "r") as file:
+        return file.readlines()
 
-solve2(example)
-# solve2(get_input('day1/input.txt'))
+
+def test_solutions():
+    input_example = get_input("day1/example.txt")
+    input_final = get_input("day1/input.txt")
+
+    assert solve(input_example) == 3
+    assert solve(input_final) == 984
+    assert solve2(input_example) == 6
+    assert solve2(input_final) == 5657
