2e7144aa3103d30d204343a9e19e92861d36c821
Alex Howard <thezanke@gmail.com>
2025-12-06 11:28:51 -0500

    day6 part2

--- day6/day6.py
+++ day6/day6.py
@@ -3,7 +3,7 @@ import pytest
 
 
 def solve_part_1(
-    input: tuple[tuple[str, tuple[int, int], list[int]], list[str]],
+    input: tuple[list[tuple[str, tuple[int, int], list[list[str]]]], list[str]],
 ) -> int:
     cols, lines = input
 
@@ -22,21 +22,21 @@ def solve_part_1(
 
 
 def solve_part_2(
-    input: tuple[tuple[str, tuple[int, int], list[str]], list[str]],
+    input: tuple[list[tuple[str, tuple[int, int], list[list[str]]]], list[str]],
 ) -> int:
     problems, lines = input
-
-    for line in lines:
-        for i, p in enumerate(problems):
-            [start, end] = p[1]
-
-            value = line[start:end].strip()
-            if value:
-                p[2].append(value)
-
     t = 0
+
     for p in problems:
-        t += eval(p[0].join(p[2]))
+        span = p[1]
+        for line in lines:
+            value = line[span[0] : span[1]]
+            if value:
+                value = list(value)
+                value.reverse()
+                p[2].append(value)
+        rotated = ["".join(row).strip() for row in list(zip(*p[2]))[::-1]]
+        t += eval(p[0].join(filter(None, rotated)))
 
     return t
 
@@ -57,22 +57,21 @@ def get_input(file_path):
         return (problems, lines)
 
 
-input_example = get_input("day6/example.txt")
-input_final = get_input("day6/input.txt")
-
-
 def test_part_1_example_solution():
+    input_example = get_input("day6/example.txt")
     assert solve_part_1(input_example) == 4277556
 
 
 def test_part_1_final_solution():
+    input_final = get_input("day6/input.txt")
     assert solve_part_1(input_final) == 6757749566978
 
 
 def test_part_2_example_solution():
+    input_example = get_input("day6/example.txt")
     assert solve_part_2(input_example) == 3263827
 
 
-@pytest.mark.skip(reason="n/i")
 def test_part_2_final_solution():
-    assert solve_part_2(input_final) == None
+    input_final = get_input("day6/input.txt")
+    assert solve_part_2(input_final) == 10603075273949
