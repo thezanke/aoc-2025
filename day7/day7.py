6b5aa06328218e44b094649d2788bd64d36bd961
Alex Howard <thezanke@gmail.com>
2025-12-07 17:35:47 -0500

    clean

--- day7/day7.py
+++ day7/day7.py
@@ -1,5 +1,4 @@
 import itertools
-import pytest
 
 
 def solve_part_1(lines: list[list[str]]) -> int:
@@ -7,25 +6,40 @@ def solve_part_1(lines: list[list[str]]) -> int:
 
     for a, b in itertools.pairwise(lines):
         for ii in range(len(a)):
-            if a[ii] == "S" or a[ii] == "|":
-                if b[ii] == "^":
-                    if b[ii - 1] == ".":
-                        b[ii - 1] = "|"
-                    if b[ii + 1] == ".":
-                        b[ii + 1] = "|"
-                    t += 1
-                else:
-                    b[ii] = "|"
+            if (a[ii] == "|") and b[ii] == "^":
+                t += 1
     return t
 
 
-def print_lines(lines: list[list[str]]):
-    for line in lines:
-        print("".join(line))
-    print()
-
-
 def solve_part_2(lines: list[list[str]]) -> int:
+    cache: dict[tuple[int, int], int] = {}
+
+    def solve_recursively(lines: list[list[str]], x, y=0):
+        if (x, y) in cache:
+            return cache[(x, y)]
+
+        if y >= len(lines):
+            v = 1
+        elif lines[y][x] == "|":
+            v = solve_recursively(lines, x, y + 1)
+        elif lines[y][x] == "^":
+            v = solve_recursively(lines, x - 1, y + 1) + solve_recursively(
+                lines, x + 1, y + 1
+            )
+
+        cache[(x, y)] = v
+
+        return v
+
+    return solve_recursively(lines, lines[0].index("|"))
+
+
+###
+# UNIT TESTS
+###
+
+
+def parse_input(lines: list[list[str]]) -> list[list[str]]:
     for a, b in itertools.pairwise(lines):
         for ii in range(len(a)):
             if a[ii] == "S" or a[ii] == "|":
@@ -36,43 +50,13 @@ def solve_part_2(lines: list[list[str]]) -> int:
                         b[ii + 1] = "|"
                 else:
                     b[ii] = "|"
-
-    cache: dict[tuple[int, int], int] = {}
-
-        
-    def solve_recursively(lines: list[list[str]], x, y=0):
-        if (x, y) in cache:
-            return cache[(x, y)]
-
-        v: int = 0
-        if y >= len(lines):
-            v += 1
-        elif lines[y][x] == "|" or lines[y][x] == "S":
-            v += solve_recursively(lines, x, y + 1)
-        elif lines[y][x] == "^":
-            v += solve_recursively(lines, x - 1, y + 1) + solve_recursively(
-                lines, x + 1, y + 1
-            )
-
-        cache[(x, y)] = v
-
-        return v
-
-        
-
-    t = solve_recursively(lines, lines[0].index("S"))
-
-    return t
-
-
-###
-# UNIT TESTS
-###
+    return lines[1:]
 
 
 def get_input(file_path):
     with open(file_path, "r") as file:
-        return [list(line) for line in file.read().splitlines()]
+        lines = [list(line) for line in file.read().splitlines()]
+        return parse_input(lines)
 
 
 def test_part_1_example_solution():
